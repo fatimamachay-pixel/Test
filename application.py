e1790f01bb4112d78c60ab470e4acbacdd49c05f
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-25 10:06:21 +0100

    renomer le fiche d'application + ajouter la 1er fonction resultat

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from Données import produits,prix
+from Les_fonctions import resultat
 print(produits,prix)
-
+print(resultat)
 #association chaque produit à son prix :
 resultat=zip(produits,prix)
 print(list(resultat))
