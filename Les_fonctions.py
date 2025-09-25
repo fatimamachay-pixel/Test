041d06fef7f756ab6f208c8cbeba9681e16ce4d2
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-25 13:00:07 +0100

    application de la fonction transformer sur la fiche application

--- Les_fonctions.py
+++ Les_fonctions.py
@@ -2,4 +2,11 @@ def fonction1(a,b):
     return list(zip(a,b))
 
 def filter_sup(l):
-    return (filter(lambda p: p[1]>=30 ,l))
+    return list(filter(lambda p:p[1]>=30,l))
+
+def tansformer(produits):
+    r1=[]
+    for a in produits :
+        produit_finaux= (f"le produit {a[0]} cout {a[1]}DH")  
+        r1.append(produit_finaux)
+    return r1
--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from Données import produits,prix
-from Les_fonctions import fonction1,filter_sup
+from Les_fonctions import fonction1,filter_sup,tansformer
+print(tansformer)
+
 print(filter_sup)
 print(produits,prix)
 association=fonction1(produits,prix)
 print(association)
-# #filtred :
-# filter_sup_30=list(filter_sup(association))
-# print(filter_sup_30)
-
 filter_fct=filter_sup(association)
 print(filter_fct)
+X=tansformer(association)
+print(X)
