b1ceb6c701522d402c326e5dcfcc7ccd2382da70
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-25 11:29:31 +0100

    resoudre la problematique 'object is not subnot subscriptable'

--- Les_fonctions.py
+++ Les_fonctions.py
@@ -1,2 +1,5 @@
 def fonction1(a,b):
     return list(zip(a,b))
+
+def filter_sup(l):
+    return list(filter(lambda c: c >= 30, l))
--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 from Données import produits,prix
 from Les_fonctions import fonction1
-
+from Les_fonctions import filter_sup
+print(filter_sup)
 print(produits,prix)
-print(fonction1(produits,prix))
+association=fonction1(produits,prix)
+print(association)
+filter_sup_30=filter_sup(prix)
 
-#association chaque produit à son prix :
-# resulatat=fonction1(produits,prix)
-# print(resultat)
+
+
