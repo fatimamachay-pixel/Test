6c99bed3e94907e1a40f1594df19f23b1b43e4da
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-25 09:41:28 +0100

    ajouter les données

---
+++ Données.py
@@ -0,0 +1,3 @@
+produits=["Clavier", "Souris","Ecran","Chaise","Bureau","Lampe"]
+prix=[45,150,1200,855,2000,25]
+
---
+++ test2.py
@@ -0,0 +1,6 @@
+from Données import produits,prix
+association=[]
+#association chaque produit à son prix:
+association= list(zip([produits],[prix]))
+print(association)
+
