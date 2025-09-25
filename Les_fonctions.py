9159e00d305801a5b48c1a47aa657607cf8c3697
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-25 12:25:26 +0100

    ajouter la fonction/ transformer dans la fiche les fonctions

--- Les_fonctions.py
+++ Les_fonctions.py
@@ -2,4 +2,4 @@ def fonction1(a,b):
     return list(zip(a,b))
 
 def filter_sup(l):
-    return list(filter(lambda c: c >= 30, l))
+    return (filter(lambda p: p[1]>=30 ,l))
--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 from Données import produits,prix
-from Les_fonctions import fonction1
-from Les_fonctions import filter_sup
+from Les_fonctions import fonction1,filter_sup
 print(filter_sup)
 print(produits,prix)
 association=fonction1(produits,prix)
 print(association)
-filter_sup_30=filter_sup(prix)
+# #filtred :
+# filter_sup_30=list(filter_sup(association))
+# print(filter_sup_30)
 
-
-
+filter_fct=filter_sup(association)
+print(filter_fct)
